8224649ed46b08381c2577e4f86898ad2112b7e2
Jan--Henrik <janhenrik@janhenrik.org>
2020-11-08 21:16:31 +0100

    added more images

--- firmware/Inc/images/image_header.h
+++ firmware/Inc/images/image_header.h
@@ -4,11 +4,14 @@
 #include "cal_i.h"
 #include "cnt.h"
 #include "cal_n.h"
+#include "bar.h"
 #include "tara_red.h"
 #include "cnt_i.h"
+#include "bb.h"
 #include "tara_green.h"
 #include "gram.h"
 #include "tara.h"
+#include "bar_i.h"
 #include "gram_i.h"
 
 #endif
